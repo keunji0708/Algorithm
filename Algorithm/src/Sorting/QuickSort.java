a2661e8274362f78e3e97ed8b3a20e195200d05f
keunj <keunj@DESKTOP-3FP78GL>
2018-12-31 22:57:44 +0900

    read "data04.txt" and print

--- Algorithm/src/Sorting/QuickSort.java
+++ Algorithm/src/Sorting/QuickSort.java
@@ -1,5 +1,33 @@
 package Sorting;
 
-public class QuickSort {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
+public class QuickSort {
+	public static void main(String args[]) throws IOException{
+		ArrayList<Integer> list = new ArrayList<Integer>();		
+		BufferedReader br = new BufferedReader(new FileReader("data04.txt"));
+		String line = br.readLine();
+		
+		while (line != null) {
+			StringTokenizer st = new StringTokenizer(line, ",");
+			while (st.hasMoreTokens()) {
+				int num = Integer.parseInt(st.nextToken());
+				list.add(num);
+			}
+			line = br.readLine();
+		}
+		//quickSort();
+		
+		for(int i = 0; i < list.size(); i++) {
+			if (i == list.size() - 1) {
+				System.out.print(list.get(i));
+			} else {
+				System.out.print(list.get(i) + ",");
+			}
+		}
+	}
 }
